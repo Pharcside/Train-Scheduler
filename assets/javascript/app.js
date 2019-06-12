248734cba5dead8856b2cfabcb2f4fc21110a92a
Brian Corbett <briancorbett@brighthouse.com>
2019-06-12 16:24:58 -0400

    removed firebase config from Index.html and added it to app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBQ4hwU_BKw5Py6Afdf5UyRdheIPeMGHrY",
+    authDomain: "trainscheduleproject-18d88.firebaseapp.com",
+    databaseURL: "https://trainscheduleproject-18d88.firebaseio.com",
+    projectId: "trainscheduleproject-18d88",
+    storageBucket: "",
+    messagingSenderId: "667608258919",
+    appId: "1:667608258919:web:5720e885792c876f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
